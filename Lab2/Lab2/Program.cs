cf4f662d1956ebc257042c65e4741aaeded753d8
jony371400 <jony371400@gmail.com>
2017-06-03 16:28:30 +0800

    10311128

---
+++ Lab2/Lab2/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (StreamReader sr = new StreamReader(@"..\..\..\..\Data\Input.txt"))
+            {
+                while (sr.Peek() != -1)
+                {
+                    string line = sr.ReadLine();
+                    foreach (var ch in line)
+                    {
+                        if (ch.ToString() == ".")
+                        {
+                            Console.Write(".\n");
+                            continue;
+                        }
+                        if (ch.ToString() == "?")
+                        {
+                            Console.Write("?\n");
+                            continue;
+                        }
+                        if (ch.ToString() == "!")
+                        {
+                            Console.Write("!\n");
+                            continue;
+                        }
+                        else
+                            Console.Write(ch);
+                    }
+                }
+            }
+        }
+    }
+}
+
+
